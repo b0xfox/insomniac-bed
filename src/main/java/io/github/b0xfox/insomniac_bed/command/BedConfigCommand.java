4a7672df90d34a8dd3cb3ecb684d461e1a58f5c9
b0xfox <172937284+b0xfox@users.noreply.github.com>
2026-09-01 10:54:55 -0400

    fix: stopped sending command feedback depending upon context

--- src/main/java/io/github/b0xfox/insomniac_bed/command/BedConfigCommand.java
+++ src/main/java/io/github/b0xfox/insomniac_bed/command/BedConfigCommand.java
@@ -1,5 +1,7 @@
 package io.github.b0xfox.insomniac_bed.command;
 
+import java.util.Optional;
+
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -13,6 +15,7 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.world.GameRules;
 
 public class BedConfigCommand {
 
@@ -22,36 +25,38 @@ public class BedConfigCommand {
 
                         .then(
                                 CommandManager.literal("darkness")
-                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool()))
-                                        .executes(BedConfigCommand::runToggleDarkness))
+                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool())
+                                                .executes(BedConfigCommand::runToggleDarkness)))
 
                         .then(
                                 CommandManager.literal("advance_time")
-                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool()))
-                                        .executes(BedConfigCommand::runToggleTimeSkipping))
+                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool())
+                                                .executes(BedConfigCommand::runToggleTimeSkipping)))
                         .then(
                                 CommandManager.literal("limits")
-                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool()))
-                                        .executes(BedConfigCommand::runToggleLimits))
+                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool())
+                                                .executes(BedConfigCommand::runToggleLimits)))
 
                         .then(
                                 CommandManager.literal("gui")
-                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool()))
-                                        .executes(BedConfigCommand::runToggleGUI)));
+                                        .then(CommandManager.argument("enabled", BoolArgumentType.bool())
+                                                .executes(BedConfigCommand::runToggleGUI))));
     }
 
     public static int runRoot(CommandContext<ServerCommandSource> ctx) {
 
-        ServerPlayerEntity serverPlayer = ctx.getSource().getPlayer();
+        ServerCommandSource source = ctx.getSource();
+        ServerPlayerEntity player = source.getPlayer();
 
-        if (serverPlayer != null) {
-            BedConfigState state = BedConfigState.getServerState(serverPlayer.getServerWorld());
-            BedConfigData data = state.getByUUID(serverPlayer.getUuid());
+        if (player != null) {
+
+            BedConfigState state = BedConfigState.getServerState(player.getServerWorld());
+            BedConfigData data = state.getByUUID(player.getUuid());
 
             if (state == null || data == null)
                 return 0;
 
-            listConfigData(serverPlayer, data);
+            listConfigData(source, data);
 
             return 1;
         }
@@ -61,133 +66,94 @@ public class BedConfigCommand {
 
     public static int runToggleDarkness(CommandContext<ServerCommandSource> ctx) {
 
-        ServerPlayerEntity serverPlayer = ctx.getSource().getPlayer();
+        boolean enabled = BoolArgumentType.getBool(ctx, "enabled");
 
-        if (serverPlayer != null) {
-            BedConfigState state = BedConfigState.getServerState(serverPlayer.getServerWorld());
-            BedConfigData data = state.getByUUID(serverPlayer.getUuid());
+        ServerCommandSource source = ctx.getSource();
+        Optional<BedConfigData> data = BedConfig.updateOption(source.getPlayer(), BedConfig.Option.DARKNESS, enabled);
 
-            if (state == null || data == null)
-                return 0;
-
-            BedConfigData updatedData = new BedConfigData(
-                    !data.darknessEnabled(),
-                    data.timeEnabled(),
-                    data.limitsEnabled(),
-                    data.guiEnabled());
-
-            state.setByUUID(serverPlayer.getUuid(), updatedData);
-            BedConfig.sync(serverPlayer, updatedData);
-            listConfigData(serverPlayer, updatedData);
-
-            return updatedData.darknessEnabled() ? 1 : 0;
+        if (!data.isEmpty()) {
+            listConfigData(source, data.get());
+            return enabled ? 1 : 0;
         }
 
+        source.sendMessage(Text.literal("Failed to retrieve data").formatted(Formatting.RED));
         return 0;
     }
 
     public static int runToggleTimeSkipping(CommandContext<ServerCommandSource> ctx) {
 
-        ServerPlayerEntity serverPlayer = ctx.getSource().getPlayer();
+        boolean enabled = BoolArgumentType.getBool(ctx, "enabled");
 
-        if (serverPlayer != null) {
-            BedConfigState state = BedConfigState.getServerState(serverPlayer.getServerWorld());
-            BedConfigData data = state.getByUUID(serverPlayer.getUuid());
+        ServerCommandSource source = ctx.getSource();
+        Optional<BedConfigData> data = BedConfig.updateOption(source.getPlayer(), BedConfig.Option.ADVANCE_TIME, enabled);
 
-            if (state == null || data == null)
-                return 0;
-
-            BedConfigData updatedData = new BedConfigData(
-                    data.darknessEnabled(),
-                    !data.timeEnabled(),
-                    data.limitsEnabled(),
-                    data.guiEnabled());
-
-            state.setByUUID(serverPlayer.getUuid(), updatedData);
-            BedConfig.sync(serverPlayer, updatedData);
-            listConfigData(serverPlayer, updatedData);
-
-            return updatedData.timeEnabled() ? 1 : 0;
+        if (!data.isEmpty()) {
+            listConfigData(source, data.get());
+            return enabled ? 1 : 0;
         }
 
+        source.sendMessage(Text.literal("Failed to retrieve data").formatted(Formatting.RED));
         return 0;
     }
 
     public static int runToggleLimits(CommandContext<ServerCommandSource> ctx) {
 
-        ServerPlayerEntity serverPlayer = ctx.getSource().getPlayer();
+        boolean enabled = BoolArgumentType.getBool(ctx, "enabled");
 
-        if (serverPlayer != null) {
-            BedConfigState state = BedConfigState.getServerState(serverPlayer.getServerWorld());
-            BedConfigData data = state.getByUUID(serverPlayer.getUuid());
+        ServerCommandSource source = ctx.getSource();
+        Optional<BedConfigData> data = BedConfig.updateOption(source.getPlayer(), BedConfig.Option.LIMITS, enabled);
 
-            if (state == null || data == null)
-                return 0;
-
-            BedConfigData updatedData = new BedConfigData(
-                    data.darknessEnabled(),
-                    data.timeEnabled(),
-                    !data.limitsEnabled(),
-                    data.guiEnabled());
-
-            state.setByUUID(serverPlayer.getUuid(), updatedData);
-            BedConfig.sync(serverPlayer, updatedData);
-            listConfigData(serverPlayer, updatedData);
-
-            return updatedData.limitsEnabled() ? 1 : 0;
+        if (!data.isEmpty()) {
+            listConfigData(source, data.get());
+            return enabled ? 1 : 0;
         }
 
+        source.sendMessage(Text.literal("Failed to retrieve data").formatted(Formatting.RED));
         return 0;
     }
 
     public static int runToggleGUI(CommandContext<ServerCommandSource> ctx) {
 
-        ServerPlayerEntity serverPlayer = ctx.getSource().getPlayer();
+        boolean enabled = BoolArgumentType.getBool(ctx, "enabled");
 
-        if (serverPlayer != null) {
-            BedConfigState state = BedConfigState.getServerState(serverPlayer.getServerWorld());
-            BedConfigData data = state.getByUUID(serverPlayer.getUuid());
+        ServerCommandSource source = ctx.getSource();
+        Optional<BedConfigData> data = BedConfig.updateOption(source.getPlayer(), BedConfig.Option.GUI, enabled);
 
-            if (state == null || data == null)
-                return 0;
-
-            BedConfigData updatedData = new BedConfigData(
-                    data.darknessEnabled(),
-                    data.timeEnabled(),
-                    data.limitsEnabled(),
-                    !data.guiEnabled());
-
-            state.setByUUID(serverPlayer.getUuid(), updatedData);
-            BedConfig.sync(serverPlayer, updatedData);
-            listConfigData(serverPlayer, updatedData);
-
-            return updatedData.guiEnabled() ? 1 : 0;
+        if (!data.isEmpty()) {
+            listConfigData(source, data.get());
+            return enabled ? 1 : 0;
         }
 
+        source.sendMessage(Text.literal("Failed to retrieve data").formatted(Formatting.RED));
         return 0;
     }
 
-    public static void listConfigData(ServerPlayerEntity player, BedConfigData data){
+    public static void listConfigData(ServerCommandSource source, BedConfigData data) {
 
-        if (player == null || data == null)
+        if (source == null || data == null)
             return;
 
-        player.sendMessage(Text
-        .literal("Bed Config Options").formatted(Formatting.YELLOW)
-        .append(Text.literal(" -- ").formatted(Formatting.YELLOW))
-        .append(Text.literal(player.getName().getString()).formatted(Formatting.WHITE))
-        .append(Text.literal("\n> ").formatted(Formatting.WHITE))
-            .append(Text.literal("Darkness: ").formatted(Formatting.GOLD))
-            .append(data.darknessEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
-        .append(Text.literal("\n> ").formatted(Formatting.WHITE))
-            .append(Text.literal("Time Skipping: ").formatted(Formatting.GOLD))
-            .append(data.timeEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
-        .append(Text.literal("\n> ").formatted(Formatting.WHITE))
-            .append(Text.literal("Sleep Limitations: ").formatted(Formatting.GOLD))
-            .append(data.limitsEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
-        .append(Text.literal("\n> ").formatted(Formatting.WHITE))
-            .append(Text.literal("Leave Bed GUI: ").formatted(Formatting.GOLD))
-            .append(data.guiEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
-        );
+        boolean sendCommandFeedback = source.getWorld().getGameRules().getBoolean(GameRules.SEND_COMMAND_FEEDBACK);
+
+        if (source.getEntity() instanceof ServerPlayerEntity || sendCommandFeedback == true) {
+
+            source.sendMessage(Text
+                    .literal("Bed Config Options").formatted(Formatting.YELLOW)
+                    .append(Text.literal(" -- ").formatted(Formatting.YELLOW))
+                    .append(Text.literal(source.getName()).formatted(Formatting.WHITE))
+                    .append(Text.literal("\n> ").formatted(Formatting.WHITE))
+                    .append(Text.literal("Darkness: ").formatted(Formatting.GOLD))
+                    .append(data.darknessEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
+                    .append(Text.literal("\n> ").formatted(Formatting.WHITE))
+                    .append(Text.literal("Time Skipping: ").formatted(Formatting.GOLD))
+                    .append(data.timeEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
+                    .append(Text.literal("\n> ").formatted(Formatting.WHITE))
+                    .append(Text.literal("Sleep Limitations: ").formatted(Formatting.GOLD))
+                    .append(data.limitsEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED))
+                    .append(Text.literal("\n> ").formatted(Formatting.WHITE))
+                    .append(Text.literal("Leave Bed GUI: ").formatted(Formatting.GOLD))
+                    .append(data.guiEnabled() ? Text.literal("true").formatted(Formatting.GREEN) : Text.literal("false").formatted(Formatting.RED)));
+        }
+
     }
 }
--- src/main/java/io/github/b0xfox/insomniac_bed/data/BedConfig.java
+++ src/main/java/io/github/b0xfox/insomniac_bed/data/BedConfig.java
@@ -1,13 +1,66 @@
 package io.github.b0xfox.insomniac_bed.data;
 
+import java.util.Optional;
+
+import io.github.b0xfox.insomniac_bed.command.BedConfigCommand;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class BedConfig {
 
+    public enum Option {
+        ADVANCE_TIME,
+        DARKNESS,
+        LIMITS,
+        GUI
+    }
+
     public static void initialize() {
         PayloadTypeRegistry.playS2C().register(BedConfigPayload.ID, BedConfigPayload.CODEC);
+    }
+
+    public static Optional<BedConfigData> updateOption(ServerPlayerEntity player, BedConfig.Option option, boolean enabled){
+
+        if (player == null) return Optional.empty();
+
+        BedConfigState state = BedConfigState.getServerState(player.getServerWorld());
+        BedConfigData data = state.getByUUID(player.getUuid());
+
+        if (state == null || data == null) return Optional.empty();
+
+        boolean darkness = data.darknessEnabled();
+        boolean time = data.timeEnabled();
+        boolean limits = data.limitsEnabled();
+        boolean gui = data.guiEnabled();
+
+        switch (option) {
+
+            case ADVANCE_TIME:
+                time = enabled;
+                break;
+
+            case DARKNESS:
+                darkness = enabled;
+                break;
+
+            case LIMITS:
+                limits = enabled;
+                break;
+
+            case GUI:
+                gui = enabled;
+                break;
+
+            default:
+                return Optional.empty();
+        }
+
+        BedConfigData updatedData = new BedConfigData(darkness, time, limits, gui);
+        state.setByUUID(player.getUuid(), updatedData);
+        BedConfig.sync(player, updatedData);
+
+        return Optional.of(updatedData);
     }
 
     public static boolean isDarknessEnabled(ServerPlayerEntity player) {
